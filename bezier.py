376288bc34f665064a0a8b8c54f300b1ca21c4fb
Karl Jakob Larsson <k.jakob.larsson@gmail.com>
2016-05-01 16:18:28 +0200

    Implemented a cubic bezier in Python.

---
+++ bezier.py
@@ -0,0 +1,23 @@
+#! /usr/env python
+
+from mathutils import *
+import matplotlib.pyplot as plt
+import numpy
+
+P = [Vector([0,0]), Vector([1,0]), Vector([1,1]), Vector([0,1])]
+
+
+def cubic_bezier(t):
+    return pow((1-t),3)*P[0] + 3*pow((1-t),2)*t*P[1] + 3*(1-t)*pow(t,2)*P[2] + pow(t,3)*P[3]
+
+def plot():
+
+    t = list(numpy.linspace(0.4,1,100))
+
+    res = list(map(cubic_bezier, t))
+
+    x = list(map(lambda s: s[0], res))
+    y = list(map(lambda s: s[1], res))
+
+    plt.plot(x, y)
+    plt.show()
